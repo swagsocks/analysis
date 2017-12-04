d98deac301699884c6b7790070f10bda4b2e12dd
CameronH <cameron.holl@gmail.com>
2017-12-03 21:03:39 -0800

    Pickle Articles
    Updated pickle process
    Current classifiers on 100 samples of each return 76%

--- political/Political_sentiment.py
+++ political/Political_sentiment.py
@@ -1,12 +1,22 @@
 ###HTML PARSING#####################
 ### Thankyou NewsAPI and BeautifulSoup
 ### CNN API Broken, Fox News no API
-
-
 import requests
 from bs4 import BeautifulSoup
 import urllib2
 import pickle
+
+
+#democrat = []
+news_urls = open("cnn_articles.pickle", "rb")
+democrat = pickle.load(news_urls)
+news_urls.close()
+####FOX
+#republican = []
+news_urls = open("fox_articles.pickle", "rb")
+republican = pickle.load(news_urls)
+news_urls.close()
+
 
 #NewsAPI
 ###CNN###
@@ -17,7 +27,6 @@ cnn_url_list = []
 for x in cnn_articles:
 	cnn_url_list.append(x['url'])
 
-democrat = []
 for url in cnn_url_list:
 #html reader
 	try:
@@ -28,6 +37,7 @@ for url in cnn_url_list:
 	body = soup.find_all('div', {'class':'zn-body__paragraph'})
 	one_list = []
 	for containers in body:
+		print containers
 		one_list.append(containers.get_text().encode('ascii', 'ignore'))
 	one_string= ''.join(one_list)
 	democrat.append(one_string)
@@ -40,7 +50,6 @@ fox_url_list = []
 for x in fox_articles:
 	fox_url_list.append(x['url'])
 
-republican = []
 for url in fox_url_list:
 	try:
 		html_doc = urllib2.urlopen(url).read()
@@ -54,11 +63,8 @@ for url in fox_url_list:
 		one_list.append(containers.get_text().encode('ascii', 'ignore'))
 	one_string= ''.join(one_list)
 	republican.append(one_string)
-  
-  
-  ####Classification#################
-  
-  import nltk
+
+import nltk
 from nltk import word_tokenize
 from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
@@ -67,6 +73,7 @@ from sklearn.svm import SVC, LinearSVC, NuSVC
 from nltk.classify import ClassifierI
 from statistics import mode
 import random
+import pickle
 
 class VoteClassifier(ClassifierI):
 	def __init__(self, *classifiers):
@@ -96,15 +103,15 @@ def find_features(document):
 	return features
 
 ####CNN 
-#democrat = []
-news_urls = open("cnn_articles.pickle", "rb")
-democrat = pickle.load(news_urls)
-news_urls.close()
-####FOX
-#republican = []
-news_urls = open("fox_articles.pickle", "rb")
-republican = pickle.load(news_urls)
-news_urls.close()
+
+
+cnn_document = open("cnn_articles.pickle", "wb")
+pickle.dump(democrat, cnn_document)
+cnn_document.close()
+
+fox_document = open("fox_articles.pickle", "wb")
+pickle.dump(republican, fox_document)
+fox_document.close()
 
 
 documents = []
@@ -127,7 +134,7 @@ for word in republican_words:
 	
 all_words = nltk.FreqDist(all_words)
 
-word_features = list(all_words.keys())[:4000]
+word_features = list(all_words.keys())[:5500]
 
 featuresets = [(find_features(article), category) for (article, category) in documents]
 random.shuffle(featuresets)
@@ -139,15 +146,51 @@ testing_set = featuresets[int(len(featuresets)*0.9):]
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 print("Original NBA accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
 
-classifier.show_most_informative_features(50)
-
-cnn_document = open("cnn_articles.pickle", "wb")
-pickle.dump(democrat, cnn_document)
-cnn_document.close()
-
-fox_document = open("fox_articles.pickle", "wb")
-pickle.dump(republican, fox_document)
-fox_document.close()
+classifier.show_most_informative_features(100)
 
 
-#######
+
+
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+# GaussianNB_classifier = SklearnClassifier(GaussianNB())
+# GaussianNB_classifier.train(training_set)
+# print("GaussianNB_classifier accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+BernoulliNB_classifier.train(training_set)
+print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+
+voted_classifier = VoteClassifier(classifier, MNB_classifier, BernoulliNB_classifier, LogisticRegression_classifier, SGDClassifier_classifier, SVC_classifier, LinearSVC_classifier, NuSVC_classifier)
+
+print("Voted_classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
+
+
+
